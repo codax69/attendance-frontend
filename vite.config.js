6b587d88929c7f479907a0b546b6d142cfe28a5d
codax69 <pritbusiness03@gmail.com>
2024-08-04 21:47:30 +0530

    beta test 1.0.0

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/server': 'https://attendance-app-production-ec33.up.railway.app',
-      '/sheet': 'https://script.google.com',
+      '/server': {
+        target: 'https://attendance-app-t0x1.onrender.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/server/, ''),
+      },
+      '/sheet': {
+        target: 'https://script.google.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/sheet/, ''),
+      },
     },
   },
 });
