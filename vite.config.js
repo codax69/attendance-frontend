5f3ff540ff7fffc9d5766ff49dfbd2c34630c3f0
Pritesh Patel <pritbusiness03@gmail.com>
2024-09-11 13:31:08 +0530

    1.45

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://attendance-app-1.onrender.com/",
+        target: "http://localhost:3000/",
         changeOrigin: true,
         // rewrite:(path)=>path.replace(/^\/api/,"")
       },
