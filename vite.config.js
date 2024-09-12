b18187440695fb440470a3759e0aa131aad63934
Pritesh Patel <pritbusiness03@gmail.com>
2024-09-12 13:08:17 +0530

    fix cors issue A1

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "https://attendance-app-1.onrender.com",
         changeOrigin: true,
       },
       "/macros": {
