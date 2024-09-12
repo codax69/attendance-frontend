c011cea27f1933d224353528ef2e20d8b05d913e
Pritesh Patel <pritbusiness03@gmail.com>
2024-09-12 10:52:15 +0530

    changes in vite.conf

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,14 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "https://attendance-app-1.onrender.com/",
         changeOrigin: true,
         rewrite:(path)=>path.replace(/^\/api/,"/api")
       },
       "/macros": {
         target: "https://script.google.com/",
         changeOrigin: true,
-        rewrite:(path)=>path.replace(/^\/macros/,"/macros")
+        rewrite:(path)=>path.replace(/^\/macros/,"/api")
 
       },
       "/geo": {
